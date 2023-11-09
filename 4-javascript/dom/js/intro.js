d9dc2bc8e0940e2f93b4be9a4719bfa79e9f38d2
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-11-09 11:56:19 +0630

    dom start

---
+++ 4-javascript/dom/js/intro.js
@@ -0,0 +1,39 @@
+
+
+// const area = (w,b) => {
+//     return w * b;
+// }
+
+// const name = window.prompt("what is your name ?")
+// window.alert("My name is "+name)
+
+// console.log(window);
+
+// const width = window.prompt("Your room width in ft")
+// const breadth = window.prompt("Your room breadth in ft")
+// window.alert(`Your room area is ${area(width,breadth)} sqft`)
+
+
+// const result = window.confirm("Are U ready to go to school ?")
+// console.log(result ? "I'm ready" : "not yet");
+
+// window.print()
+
+
+console.log(document);
+
+// selectors
+
+const heading = document.getElementById("heading");
+const para = document.getElementById("para");
+const googleLink = document.getElementById("googleLink");
+const listGroup = document.getElementById("listGroup");
+const input = document.getElementById("input");
+const btn = document.getElementById("btn");
+
+console.log(heading);
+console.log(para);
+console.log(googleLink);
+console.log(listGroup);
+console.log(input);
+console.log(btn);
