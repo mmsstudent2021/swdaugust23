5711aa288464fbfde5ebdba6c5337d169b0dbd99
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-11-15 12:13:07 +0630

    todo start

---
+++ 4-javascript/todo/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html","./src/**/*.{js,html}"],
+  theme: {
+    fontFamily : {
+      'sans': ['Montserrat', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
